4571df78138ca347de436945920a8d7d5bfe4ac3
Ryleeeee <gregcalleri13@gmail.com>
2024-02-19 11:50:23 +0100

    metroveloce

---
+++ main.js
@@ -0,0 +1,10 @@
+function massiveCreate()
+{
+    var notches = document.getElementsByClassName("speedometer-notch");
+    var i = 1;
+    for (const index of notches)
+    {
+        index.classList.add(`notch${i}`);
+        i++;
+    }
+}
