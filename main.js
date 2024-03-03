b0c63ed21437e3c122e423ceb3c2c4dd4c38b758
S.T. Grun Kutsie <73830809+stgrunkutsie@users.noreply.github.com>
2024-03-03 23:25:06 +0100

    iShowSPID
    baby dont hurt me
    dont hurt me
    no more

--- main.js
+++ main.js
@@ -1,15 +1,56 @@
-function massiveCreate()
-{
-    var notches = document.getElementsByClassName("speedometer-notch");
-    var i = 1;
-    for (const index of notches)
+const numberOfNotches = 19;
+
+function massiveCreate(){
+    let container = document.getElementsByClassName("speedometer-container")[0];   
+    for(let i = 1; i<= numberOfNotches+1 ;i++)
     {
-        index.classList.add(`notch${i}`);
-        i++;
+        let div = document.createElement('div');
+        div.classList.add("speedometer-notch");
+        div.classList.add("notch" + i);
+        container.appendChild(div);
     }
 }
 
-function setSpeed(speed)
-{
-    
+function setSpeed_GIGACHAD(speed) {
+    const arrowWrapper = document.querySelector('.arrow-wrapper');
+    const notches = document.querySelectorAll('.speedometer-notch');
+
+    arrowWrapper.style.transform = `rotate(${speed}deg)`; 
+
+    if (speed == 30)
+    {
+        for (let i = 0; i < 4; i++)
+        {
+            notches[i].classList.add("active");
+        }
+        for (let i = 4; i < 19; i++)
+        {
+            notches[i].classList.remove("active");
+        }
+    }
+    else if (speed == 90)
+    {
+        for (let i = 0; i < 10; i++)
+        {
+            notches[i].classList.add("active");
+        }
+        for (let i = 10; i < 19; i++)
+        {
+            notches[i].classList.remove("active");
+        }
+    }
+    else if (speed == 180)
+    {
+        for (let i = 0; i < 19; i++)
+        {
+            notches[i].classList.add("active");
+        }
+    }
+    else
+    {
+        for (let i = 0; i < 19; i++)
+        {
+            notches[i].classList.remove("active");
+        }
+    }
 }
